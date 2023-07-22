dab481ea50b7d222c12a24667b860f98a44e54c4
Soimn <soimn.dok@gmail.com>
2023-07-22 19:35:08 +0200

    day 12 improved

--- day12.c
+++ day12.c
@@ -4,87 +4,6 @@
 
 #include "rin.h"
 
-typedef struct Map_Cell
-{
-  R_V2S prev;
-  R_uint path_length;
-  R_uint height;
-  R_bool can_visit;
-} Map_Cell;
-
-void
-InitMap(R_String input, Map_Cell* map, R_uint map_width, R_uint map_height, R_V2S* start_pos, R_V2S* end_pos)
-{
-  for (R_uint j = 0; j < map_height; ++j)
-  {
-    for (R_uint i = 0; i < map_width; ++i)
-    {
-      R_u8 c = input.data[j*(map_width + 2) + i];
-
-      if (c == 'S')
-      {
-        *start_pos = R_V2S(i, j);
-        c = 'a';
-      }
-      else if (c == 'E')
-      {
-        *end_pos = R_V2S(i, j);
-        c = 'z';
-      }
-
-      map[j*map_width + i] = (Map_Cell){ .height = c - 'a', .can_visit = R_true };
-    }
-  }
-}
-
-R_uint
-RunBFSToEnd(Map_Cell* map, R_uint map_width, R_uint map_height, R_V2S start_pos, R_V2S end_pos)
-{
-  R_uint visiting_ringbuffer_size = map_width*map_height;
-  R_V2S* visiting_ringbuffer = malloc(sizeof(R_V2S)*visiting_ringbuffer_size);
-  R_uint visiting_head = 0;
-  R_uint visiting_tail = 0;
-
-  visiting_ringbuffer[visiting_tail++] = start_pos;
-
-  for (;;)
-  {
-    R_ASSERT(visiting_head != visiting_tail);
-    R_V2S current_pos = visiting_ringbuffer[visiting_head];
-    visiting_head = (visiting_head + 1) % visiting_ringbuffer_size;
-
-    if (R_V2S_Match(current_pos, end_pos)) break;
-
-    Map_Cell* current_cell = &map[current_pos.y*map_width + current_pos.x];
-
-    R_V2S deltas[4] = { R_V2S(1, 0), R_V2S(0, 1), R_V2S(-1, 0), R_V2S(0, -1) };
-    for (R_uint i = 0; i < R_STATIC_ARRAY_SIZE(deltas); ++i)
-    {
-      R_V2S candidate_pos = R_V2S_Add(current_pos, deltas[i]);
-
-      if (candidate_pos.x >= 0 && candidate_pos.x < map_width &&
-          candidate_pos.y >= 0 && candidate_pos.y < map_height)
-      {
-        Map_Cell* candidate_cell = &map[candidate_pos.y*map_width + candidate_pos.x];
-
-        if (candidate_cell->can_visit && current_cell->height + 1 >= candidate_cell->height)
-        {
-          candidate_cell->can_visit   = R_false;
-          candidate_cell->prev        = current_pos;
-          candidate_cell->path_length = current_cell->path_length + 1;
-
-          visiting_ringbuffer[visiting_tail] = candidate_pos;
-          visiting_tail = (visiting_tail + 1) % visiting_ringbuffer_size;
-        }
-      }
-    }
-  }
-
-  free(visiting_ringbuffer);
-
-  return map[end_pos.y*map_width + end_pos.x].path_length;
-}
-
 int
 main(int argc, char** argv)
 {
@@ -107,85 +26,136 @@ main(int argc, char** argv)
       if (input.data == 0 || fread(input.data, 1, input.size, input_file) != input_file_size) fprintf(stderr, "Failed to read input file\n");
       else
       {
-        R_uint map_width = 0;
-        for (; map_width < input.size && input.data[map_width] != '\r'; ++map_width);
+        R_V2S start_pos    = R_V2S(0, 0);
+        R_V2S end_pos      = R_V2S(0, 0);
+        R_uint* height_map;
+        R_uint map_width;
+        R_uint map_height;
 
-        R_uint map_height = input.size / (map_width + 2);
+        for (map_width = 0; input.data[map_width] != '\r'; ++map_width);
+        map_height = input.size / (map_width + 2);
 
-        R_V2S start_pos = R_V2S(0, 0);
-        R_V2S end_pos   = R_V2S(0, 0);
-
-        Map_Cell* map = malloc(sizeof(Map_Cell)*map_width*map_height);
-
-        InitMap(input, map, map_width, map_height, &start_pos, &end_pos);
-        R_uint part1_result = RunBFSToEnd(map, map_width, map_height, start_pos, end_pos);
-
-        R_uint part2_result = part1_result;
-        R_V2S best_pos      = start_pos;
+        height_map = malloc(sizeof(R_uint)*map_width*map_height);
 
         for (R_uint j = 0; j < map_height; ++j)
         {
           for (R_uint i = 0; i < map_width; ++i)
           {
-            if (input.data[j*(map_width + 2) + i] == 'a')
-            {
-              InitMap(input, map, map_width, map_height, &(R_V2S){0}, &(R_V2S){0});
-              R_uint path_length = RunBFSToEnd(map, map_width, map_height, R_V2S(i, j), end_pos);
+            R_u8 c = input.data[j*(map_width + 2) + i];
 
-              if (path_length < part2_result)
+            if (c == 'S')
+            {
+              start_pos = R_V2S(i, j);
+              c         = 'a';
+            }
+            else if (c == 'E')
+            {
+              end_pos = R_V2S(i, j);
+              c       = 'z';
+            }
+
+            height_map[j*map_width + i] = c - 'a';
+          }
+        }
+
+        R_uint visiting_ringbuffer_size = map_width*map_height;
+        R_V2S* visiting_ringbuffer      = malloc(sizeof(R_V2S)*visiting_ringbuffer_size);
+        R_uint visiting_head = 0;
+        R_uint visiting_tail = 0;
+
+        visiting_ringbuffer[visiting_tail] = start_pos;
+        visiting_tail = (visiting_tail + 1) % visiting_ringbuffer_size;
+
+        R_uint* path_length_map = calloc(map_width*map_height, sizeof(R_uint));
+        path_length_map[start_pos.y*map_width + start_pos.x] = 1;
+
+        for (;;)
+        {
+          R_ASSERT(visiting_head != visiting_tail);
+          R_V2S curr_pos = visiting_ringbuffer[visiting_head];
+          visiting_head = (visiting_head + 1) % visiting_ringbuffer_size;
+
+          R_uint curr_index = curr_pos.y*map_width + curr_pos.x;
+          R_uint curr_height = height_map[curr_index];
+          R_uint curr_len    = path_length_map[curr_index];
+
+          if (R_V2S_Match(curr_pos, end_pos))
+          {
+            printf("Part 1: %llu\n", curr_len - 1);
+            break;
+          }
+
+          R_V2S deltas[] = { R_V2S(1, 0), R_V2S(0, 1), R_V2S(-1, 0), R_V2S(0, -1) };
+          for (R_uint i = 0; i < R_STATIC_ARRAY_SIZE(deltas); ++i)
+          {
+            R_V2S cand_pos = R_V2S_Add(curr_pos, deltas[i]);
+
+            if (cand_pos.x >= 0 && cand_pos.x < map_width &&
+                cand_pos.y >= 0 && cand_pos.y < map_height)
+            {
+              R_uint cand_index = cand_pos.y*map_width + cand_pos.x;
+              R_uint cand_height = height_map[cand_index];
+              R_uint* cand_len   = &path_length_map[cand_index];
+
+              if (curr_height + 1 >= cand_height && *cand_len == 0)
               {
-                part2_result = path_length;
-                best_pos = R_V2S(i, j);
+                *cand_len = curr_len + 1;
+
+                visiting_ringbuffer[visiting_tail] = cand_pos;
+                visiting_tail = (visiting_tail + 1) % visiting_ringbuffer_size;
               }
             }
           }
         }
 
-        printf("Part 1: %llu\n", part1_result);
-        printf("Part 2: %llu\n", part2_result);
 
-        InitMap(input, map, map_width, map_height, &(R_V2S){0}, &(R_V2S){0});
-        RunBFSToEnd(map, map_width, map_height, best_pos, end_pos);
+        visiting_head = 0;
+        visiting_tail = 0;
 
-        for (R_uint i = 0; i < map_width*map_height; ++i) map[i].can_visit = R_false;
+        visiting_ringbuffer[visiting_tail] = end_pos;
+        visiting_tail = (visiting_tail + 1) % visiting_ringbuffer_size;
 
-        for (R_V2S scan = end_pos;;)
+        memset(path_length_map, 0, sizeof(R_uint)*map_width*map_height);
+        path_length_map[end_pos.y*map_width + end_pos.x] = 1;
+
+        for (;;)
         {
-          Map_Cell* scan_cell = &map[scan.y*map_width + scan.x];
-          scan_cell->can_visit = R_true;
+          R_V2S curr_pos = visiting_ringbuffer[visiting_head];
+          visiting_head = (visiting_head + 1) % visiting_ringbuffer_size;
 
-          if (R_V2S_Match(scan, best_pos)) break;
+          R_uint curr_index = curr_pos.y*map_width + curr_pos.x;
+          R_uint curr_height = height_map[curr_index];
+          R_uint curr_len    = path_length_map[curr_index];
+
+          if (curr_height == 0)
+          {
+            printf("Part 2: %llu\n", curr_len - 1);
+            break;
+          }
           else
           {
-            scan = scan_cell->prev;
-            continue;
-          }
-        }
-
-        for (R_uint j = 0; j < map_height; ++j)
-        {
-          for (R_uint i = 0; i < map_width; ++i)
-          {
-            R_V2S curr = R_V2S(i, j);
-            if (R_V2S_Match(curr, end_pos)) printf("E");
-            else
+            R_V2S deltas[] = { R_V2S(1, 0), R_V2S(0, 1), R_V2S(-1, 0), R_V2S(0, -1) };
+            for (R_uint i = 0; i < R_STATIC_ARRAY_SIZE(deltas); ++i)
             {
-              Map_Cell* cell = &map[j*map_width + i];
+              R_V2S cand_pos = R_V2S_Add(curr_pos, deltas[i]);
 
-              if (!cell->can_visit) printf(".");
-              else
+              if (cand_pos.x >= 0 && cand_pos.x < map_width &&
+                  cand_pos.y >= 0 && cand_pos.y < map_height)
               {
-                R_V2S delta = R_V2S_Sub(curr, cell->prev);
+                R_uint cand_index = cand_pos.y*map_width + cand_pos.x;
+                R_uint cand_height = height_map[cand_index];
+                R_uint* cand_len   = &path_length_map[cand_index];
 
-                if      (R_V2S_Match(delta, R_V2S( 1, 0))) printf(">");
-                else if (R_V2S_Match(delta, R_V2S( 0, 1))) printf("^");
-                else if (R_V2S_Match(delta, R_V2S(-1, 0))) printf("<");
-                else                                       printf("v");
+                if (curr_height <= cand_height + 1 && *cand_len == 0)
+                {
+                  *cand_len = curr_len + 1;
+
+                  visiting_ringbuffer[visiting_tail] = cand_pos;
+                  visiting_tail = (visiting_tail + 1) % visiting_ringbuffer_size;
+                }
               }
             }
           }
-
-          printf("\n");
         }
       }
 
